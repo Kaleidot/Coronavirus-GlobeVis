d3a217078bf92f5b170e3a76b1a50ab1a4a5f53b
Xiaoyun Hu <xiaoyunhu0812@gmail.com>
2020-02-24 21:23:28 +1100

    Animation completed

--- Assets/Unity3DGlobe/Scripts/DataLoader.cs
+++ Assets/Unity3DGlobe/Scripts/DataLoader.cs
@@ -16,11 +16,20 @@ public class DataLoader : MonoBehaviour
     public GameObject parentToAttachButtonsTo;
     public Text headDateText;
     public GameObject detailsOnDemandPanel;
+    public GameObject canvas;
 
     private string[] dates;
     private Dictionary<string, int> hashedDates = new Dictionary<string, int>();
     private SeriesArray data;
     private bool isDetailsOnDemandTriggered = false;
+    private bool isAnimated = false;
+    private bool isReset = false;
+
+    private GameObject animateButton;
+    private GameObject stopButton;
+    private GameObject resetButton;
+    private GameObject selectionPanel;
+
 
     // string array to store data     
     private Dictionary<string, string[]> hashedData = new Dictionary<string, string[]>();
@@ -44,14 +53,15 @@ public class DataLoader : MonoBehaviour
 
         data = JsonUtility.FromJson<SeriesArray>(json);
         dayVisualizer.CreateMeshes(data.AllData, 0, 4, dates[4].Split(':')[1].Replace("[", "").Replace("]", "").Trim().Split(','));
+        newCaseVisualizer.CreateMeshes(data.AllData, 0, 5, dates[4].Split(':')[1].Replace("[", "").Replace("]", "").Trim().Split(','));
 
         for (int i = 3; i < dates.Length - 1; i += 4)
         {
 
             // Extract dates from the data source
             string[] dateStrings = dates[i].Split(':');
-            string dateString = dateStrings[1].Replace("\"", "").Replace(",", "");
-            Debug.Log(dateString);
+            string dateString = dateStrings[1].Replace("\"", "").Replace(",", "").Trim();
+            //Debug.Log(dateString);
 
             // Index the dates and store them in a dictionary
             hashIndex++;
@@ -66,6 +76,9 @@ public class DataLoader : MonoBehaviour
             // Extract data from the data source
             hashedData.Add(dateString, dates[i + 1].Split(':')[1].Replace("[", "").Replace("]", "").Trim().Split(','));
         }
+
+        // set day1 date in the header
+        headDateText.text = FindDateStringByIndex(0);
 
         //// Debug dictionary
         //foreach (KeyValuePair<string, int> pair in hashedDates)
@@ -84,6 +97,93 @@ public class DataLoader : MonoBehaviour
         //    //Debug.Log("Added.");
         //}
 
+        animateButton = canvas.transform.GetChild(0).GetChild(1).GetChild(0).gameObject;
+        animateButton.GetComponent<Button>().onClick.AddListener(OnStart);
+
+        stopButton = canvas.transform.GetChild(0).GetChild(1).GetChild(1).gameObject;
+        stopButton.GetComponent<Button>().onClick.AddListener(OnStop);
+
+        resetButton = canvas.transform.GetChild(0).GetChild(1).GetChild(2).gameObject;
+        resetButton.GetComponent<Button>().onClick.AddListener(OnReset);
+
+        selectionPanel = canvas.transform.GetChild(2).gameObject;
+
+        //Debug.Log("buttons: " + animateButton.name + stopButton.name + resumeButton.name);
+
+        //Start the coroutine we define below named ExampleCoroutine.
+        //StartCoroutine(ExampleCoroutine());
+
+    }
+
+    private void RepeatFunction() {
+        int total = hashedDates.Count;
+        string date = headDateText.text;
+        int index = FindDateIndex(date);
+
+        if (index < total - 1)
+        {
+            index++;
+            string[] dataStrings = FindDataString(date);
+            headDateText.text = FindDateStringByIndex(index);
+            LoadNewData(index, dataStrings);
+        }
+        else {
+            index = 0;
+            string[] dataStrings = FindDataString(FindDateStringByIndex(index));
+            headDateText.text = FindDateStringByIndex(index);
+            LoadNewData(index, dataStrings);
+            
+        }
+        
+        
+
+        //date = FindDateStringByIndex(index);
+
+        //while (isAnimated && index < total)
+        //{
+        //    //Print the time of when the function is first called.
+        //    //Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //    string[] dataStrings = FindDataString(date);
+        //    headDateText.text = FindDateStringByIndex(index);
+
+        //    LoadNewData(index, dataStrings);
+        //    index++;
+        //    date = FindDateStringByIndex(index);
+
+        //    ////After we have waited 5 seconds print the time again.
+        //    //Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        //}
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+        int total = hashedDates.Count;
+        string date = headDateText.text;
+        int index = FindDateIndex(date);
+
+        Debug.Log("total: " + total);
+        Debug.Log("index: " + index);
+        Debug.Log("isAnimated: " + isAnimated);
+
+        while (isAnimated && index < total)
+        {
+            //Print the time of when the function is first called.
+            Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+            string[] dataStrings = FindDataString(date);
+            headDateText.text = FindDateStringByIndex(index);
+
+            LoadNewData(index, dataStrings);
+            index++;
+            date = FindDateStringByIndex(index);
+
+            //yield on a new YieldInstruction that waits for 5 seconds.
+            yield return new WaitForSeconds(2);
+
+            ////After we have waited 5 seconds print the time again.
+            Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        }
     }
 
     /// ////////////////////////////////
@@ -133,6 +233,21 @@ public class DataLoader : MonoBehaviour
             //GameObject.FindWithTag("DetailsOnDemand").SetActive(false);
             Destroy(GameObject.FindWithTag("DetailsOnDemand"));
         }
+
+        if (isAnimated)
+        {
+            animateButton.transform.GetChild(0).GetComponent<Text>().text = "Resume";
+            selectionPanel.SetActive(false);
+        }
+        else
+        {
+            selectionPanel.SetActive(true);
+        }
+
+        if (isReset)
+        {
+            animateButton.transform.GetChild(0).GetComponent<Text>().text = "Start";
+        }
     }
     /// /////////////////////////////
 
@@ -149,8 +264,45 @@ public class DataLoader : MonoBehaviour
 
             int value = FindDateIndex(dateText);
             string[] dataString = FindDataString(dateText);
+            //Debug.Log("dateText: " + dateText);
+            //foreach (string i in dataString)
+            //{
+            //    Debug.Log("dataString info: " + i);
+            //}
             LoadNewData(value, dataString);
         }
+    }
+
+    void OnStart()
+    {
+        isAnimated = true;
+        InvokeRepeating("RepeatFunction", 2f, 2f);
+    }
+
+    void OnStop()
+    {
+        isAnimated = false;
+        CancelInvoke();
+    }
+
+    void OnReset()
+    {
+        //int value = FindDateIndex("1/22/2020");
+        //Debug.Log("value: " + value);
+
+        //bool isHave = hashedData.ContainsKey(" 1/22/2020");
+        //Debug.Log("Bool: " + isHave);
+
+        string dateKey = FindDateStringByIndex(0);
+        string[] dataStrings = FindDataString(dateKey);
+        headDateText.text = dateKey;
+        //foreach (string str in dataStrings)
+        //{
+        //    Debug.Log(str);
+        //}
+
+        LoadNewData(0, dataStrings);
+        isReset = true;
     }
 
     public void CreateButton(string buttonText)
@@ -181,12 +333,36 @@ public class DataLoader : MonoBehaviour
         return dateIndex;
     }
 
+    public string FindDateStringByIndex(int index)
+    {
+        string dateKey = "";
+        if (hashedDates.ContainsValue(index))
+        {
+            foreach (KeyValuePair<string, int> pair in hashedDates)
+            {
+                if (index.Equals(pair.Value))
+                {
+                    dateKey = pair.Key;
+                    break;
+                }
+            }     
+        }
+        return dateKey;
+    }
+
     public string[] FindDataString(string dateString)
     {
-        string[] dataString = new string[100];
-        if (hashedDates.ContainsKey(dateString))
-            dataString = hashedData[dateString];
-        return dataString;
+        string[] dataStrings = new string[100];
+        if (hashedData.ContainsKey(dateString))
+        {
+            dataStrings = hashedData[dateString];
+            //foreach (string str in dataStrings)
+            //{
+            //    Debug.Log(str);
+            //}
+        }
+            
+        return dataStrings;
     }
 
     public void LoadNewData(int value, string[] dataString)
