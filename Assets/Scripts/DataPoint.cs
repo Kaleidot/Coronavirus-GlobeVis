f7b1977edc262e61513ce8a2d4dd3b39c6596636
Xiaoyun Hu <xiaoyunhu0812@gmail.com>
2020-02-28 13:47:57 +1100

    commit

---
+++ Assets/Scripts/DataPoint.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataPoint : MonoBehaviour
+{
+    private int totalNum;
+    private int newNum;
+    private string cityName;
+    private string countryName;
+
+    public int GetTotalNum()
+    {
+        return totalNum;
+    }
+
+    public void SetTotalNum(int input)
+    {
+        totalNum = input;
+    }
+
+    public int GetNewNum()
+    {
+        return newNum;
+    }
+
+    public void SetNewNum(int input)
+    {
+        newNum = input;
+    }
+
+    public string GetCityName()
+    {
+        return cityName;
+    }
+
+    public void SetCityName(string input)
+    {
+        cityName = input;
+    }
+
+    public string GetCountryName()
+    {
+        return countryName;
+    }
+
+    public void SetCountryName(string input)
+    {
+        countryName = input;
+    }
+}
